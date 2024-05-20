44c6c45966d7aebae2e622a76303ad8c1ecf766d
Michael Dalgarno-Holt <mail@lifeofmike.co.uk>
2024-05-20 01:47:42 +0100

    Task #CPS-2 - Initial solution set up

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { userState } from 'react';
+import './app.scss';
+
+
+function App () {
+  return (
+    <div className='header'>Chapelford Pet Sitting Services</div>
+  )
+}
+
+export default App
